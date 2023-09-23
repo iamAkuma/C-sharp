92e98a714c866ba348692714d8b189963924f91a
Saral Bajracharya <rmanbajra@gmail.com>
2023-09-23 17:00:32 +0545

    C# Practice

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.Write("Hey, "); //This code will not create a new line
 Console.WriteLine("Hello, World!");
+Console.Beep();
+Console.WriteLine("aKuma \n"); //This will create a space between this code and below code
+Console.WriteLine("Hello Bro!");
