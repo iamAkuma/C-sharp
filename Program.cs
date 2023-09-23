fc03fb3a73363514f5b134f1bf61ee3587bc2c42
Saral Bajracharya <rmanbajra@gmail.com>
2023-09-23 20:15:25 +0545

    C# PRACTICE

--- Program.cs
+++ Program.cs
@@ -4,30 +4,35 @@
 // Console.WriteLine("aKuma \n"); //This will create a space between this code and below code
 // Console.WriteLine("Hello Bro!");
 
-int x; //declaration
-x = 10; //init value
+// int x; //declaration
+// x = 10; //init value
 
-int y = 10;
-int z = x + y;
-Console.WriteLine(z);
+// int y = 10;
+// int z = x + y;
+// Console.WriteLine(z);
 
-int age = 21;
-double height = 177.8; //decimal number
-bool alive = true; //true or false
-char symbol = '@';
-String name = "akuma_saral";
-String userName = symbol + name;
-String unit = "cm";
+// int age = 21;
+// double height = 177.8; //decimal number
+// bool alive = true; //true or false
+// char symbol = '@';
+// String name = "akuma_saral";
+// String userName = symbol + name;
+// String unit = "cm";
 
 //constants = change garna namilos future line of code ma
 
-const double pi = 3.14;
+// const double pi = 3.14;
 
-Console.WriteLine("The value of Pi is: " + pi);
-Console.WriteLine("My name is " + name);
-Console.WriteLine("My username is: " + userName);
-Console.WriteLine("Your age is " + z);
-Console.WriteLine("Your brother's age is " + age);
-Console.WriteLine("Your height is " + height + unit);
-Console.WriteLine("Are you alive?" + alive);
-Console.WriteLine("Your symbol is " + symbol);
+// Console.WriteLine("The value of Pi is: " + pi);
+// Console.WriteLine("My name is " + name);
+// Console.WriteLine("My username is: " + userName);
+// Console.WriteLine("Your age is " + z);
+// Console.WriteLine("Your brother's age is " + age);
+// Console.WriteLine("Your height is " + height + unit);
+// Console.WriteLine("Are you alive?" + alive);
+// Console.WriteLine("Your symbol is " + symbol);
+
+double a = 3.14;
+int b = Convert.ToInt32(a); //Converts double to int
+
+Console.WriteLine(b);
