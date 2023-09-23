be5111e0b58aea3d88ff3b920120d45e8b6fce61
Saral Bajracharya <rmanbajra@gmail.com>
2023-09-23 17:40:48 +0545

    C# practice

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
-﻿Console.Write("Hey, "); //This code will not create a new line
-Console.WriteLine("Hello, World!");
-Console.Beep();
-Console.WriteLine("aKuma \n"); //This will create a space between this code and below code
-Console.WriteLine("Hello Bro!");
+﻿// Console.Write("Hey, "); //This code will not create a new line
+// Console.WriteLine("Hello, World!");
+// Console.Beep();
+// Console.WriteLine("aKuma \n"); //This will create a space between this code and below code
+// Console.WriteLine("Hello Bro!");
+
+int x; //declaration
+x = 10; //init value
+
+int y = 10;
+int z = x + y;
+Console.WriteLine(z);
+
+int age = 21;
+double height = 177.8; //decimal number
+
+Console.WriteLine("Your age is " + z);
+Console.WriteLine("Your brother's age is " + age );
+Console.WriteLine("Your height is " + height);
