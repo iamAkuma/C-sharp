7d6480b1525611fceb8a8ae9236fd723a1bcd7c9
Saral Bajracharya <rmanbajra@gmail.com>
2023-10-07 11:41:23 +0545

    Math class in C#

--- Program.cs
+++ Program.cs
@@ -81,10 +81,15 @@
 //     Console.WriteLine(friends[i]);
 // }
 
-double x = 3;
+double x = 3.99;
+double y = 5;
 
 // double a = Math.Pow(x, 2);
 // double a = Math.Sqrt(x);
-double a = Math.Abs(x);
+// double a = Math.Abs(x);
+// double a = Math.Round(x);
+// double a = Math.Ceiling(x);
+// double a = Math.Floor(x);
+double a = Math.Max(x,y);
 
 Console.WriteLine(a);
