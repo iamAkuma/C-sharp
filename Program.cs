ab9c1ebe7a0a9c568cab9a64def763b694e3ee8b
Saral Bajracharya <rmanbajra@gmail.com>
2023-10-07 15:06:15 +0545

    String methods

--- Program.cs
+++ Program.cs
@@ -1,116 +1,121 @@
-﻿// Console.Write("Hey, "); //This code will not create a new line
-// Console.WriteLine("Hello, World!");
-// Console.Beep();
-// Console.WriteLine("aKuma \n"); //This will create a space between this code and below code
-// Console.WriteLine("Hello Bro!");
+﻿// // Console.Write("Hey, "); //This code will not create a new line
+// // Console.WriteLine("Hello, World!");
+// // Console.Beep();
+// // Console.WriteLine("aKuma \n"); //This will create a space between this code and below code
+// // Console.WriteLine("Hello Bro!");
 
-// int x; //declaration
-// x = 10; //init value
+// // int x; //declaration
+// // x = 10; //init value
 
-// int y = 10;
-// int z = x + y;
-// Console.WriteLine(z);
+// // int y = 10;
+// // int z = x + y;
+// // Console.WriteLine(z);
 
-// int age = 21;
-// double height = 177.8; //decimal number
-// bool alive = true; //true or false
-// char symbol = '@';
-// String name = "akuma_saral";
-// String userName = symbol + name;
-// String unit = "cm";
+// // int age = 21;
+// // double height = 177.8; //decimal number
+// // bool alive = true; //true or false
+// // char symbol = '@';
+// // String name = "akuma_saral";
+// // String userName = symbol + name;
+// // String unit = "cm";
 
-//constants = change garna namilos future line of code ma
+// //constants = change garna namilos future line of code ma
 
-// const double pi = 3.14;
+// // const double pi = 3.14;
 
-// Console.WriteLine("The value of Pi is: " + pi);
-// Console.WriteLine("My name is " + name);
-// Console.WriteLine("My username is: " + userName);
-// Console.WriteLine("Your age is " + z);
-// Console.WriteLine("Your brother's age is " + age);
-// Console.WriteLine("Your height is " + height + unit);
-// Console.WriteLine("Are you alive?" + alive);
-// Console.WriteLine("Your symbol is " + symbol);
+// // Console.WriteLine("The value of Pi is: " + pi);
+// // Console.WriteLine("My name is " + name);
+// // Console.WriteLine("My username is: " + userName);
+// // Console.WriteLine("Your age is " + z);
+// // Console.WriteLine("Your brother's age is " + age);
+// // Console.WriteLine("Your height is " + height + unit);
+// // Console.WriteLine("Are you alive?" + alive);
+// // Console.WriteLine("Your symbol is " + symbol);
 
-// double a = 3.14;
-// int b = Convert.ToInt32(a); //Converts double to int
+// // double a = 3.14;
+// // int b = Convert.ToInt32(a); //Converts double to int
 
-// //Checking variable's data types
-// Console.WriteLine(a.GetType());
-// Console.WriteLine(b.GetType());
-// Console.WriteLine(b);
+// // //Checking variable's data types
+// // Console.WriteLine(a.GetType());
+// // Console.WriteLine(b.GetType());
+// // Console.WriteLine(b);
 
-//TypeCasting rey
+// //TypeCasting rey
 
-// int c = 321;
-// String e = Convert.ToString(c);
+// // int c = 321;
+// // String e = Convert.ToString(c);
 
-// String f = "$";
-// char g = Convert.ToChar(f);
+// // String f = "$";
+// // char g = Convert.ToChar(f);
 
-// Console.WriteLine(e.GetType());
-// Console.WriteLine(g);
-// Console.WriteLine("What is your name?");
-// String name = Console.ReadLine();
+// // Console.WriteLine(e.GetType());
+// // Console.WriteLine(g);
+// // Console.WriteLine("What is your name?");
+// // String name = Console.ReadLine();
 
-// Console.WriteLine("What is your age?");
-// int age = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine("What is your age?");
+// // int age = Convert.ToInt32(Console.ReadLine());
 
 
-// Console.WriteLine("Hello " + name);
-// Console.WriteLine("Your age is  " + age);
+// // Console.WriteLine("Hello " + name);
+// // Console.WriteLine("Your age is  " + age);
 
-// Console.ReadKey();
+// // Console.ReadKey();
 
-// int friends = 10;
-// // friends--;
-// // friends++;
-// // friends+=2;
-// // friends -=1;
-// // friends *=2;
-// // friends /=2;
-// int remainder = friends % 2;
+// // int friends = 10;
+// // // friends--;
+// // // friends++;
+// // // friends+=2;
+// // // friends -=1;
+// // // friends *=2;
+// // // friends /=2;
+// // int remainder = friends % 2;
 
-// Console.WriteLine(remainder);
+// // Console.WriteLine(remainder);
 
-// int i;
-// string[] friends = { "Friend1", "Friend2", "Friend3", "Friend4", "Friend5" };
+// // int i;
+// // string[] friends = { "Friend1", "Friend2", "Friend3", "Friend4", "Friend5" };
 
-// for (i = 0; i < friends.Length; i++)
-// {
-//     Console.WriteLine(friends[i]);
-// }
+// // for (i = 0; i < friends.Length; i++)
+// // {
+// //     Console.WriteLine(friends[i]);
+// // }
 
-// double x = 3.99;
-// double y = 5;
+// // double x = 3.99;
+// // double y = 5;
 
-// double a = Math.Pow(x, 2);
-// double a = Math.Sqrt(x);
-// double a = Math.Abs(x);
-// double a = Math.Round(x);
-// double a = Math.Ceiling(x);
-// double a = Math.Floor(x);
-// double a = Math.Max(x,y);
-// double b = Math.Min(x,y);
+// // double a = Math.Pow(x, 2);
+// // double a = Math.Sqrt(x);
+// // double a = Math.Abs(x);
+// // double a = Math.Round(x);
+// // double a = Math.Ceiling(x);
+// // double a = Math.Floor(x);
+// // double a = Math.Max(x,y);
+// // double b = Math.Min(x,y);
 
-// Console.WriteLine(b);
+// // Console.WriteLine(b);
 
-// Random random = new Random();
+// // Random random = new Random();
 
-// int num = random.Next(1,7);
-// int num1 = random.Next(1,7);
-// int num2 = random.Next(1,7);
+// // int num = random.Next(1,7);
+// // int num1 = random.Next(1,7);
+// // int num2 = random.Next(1,7);
 
-// // double num = random.NextDouble();
-// System.Console.WriteLine(num);
-// System.Console.WriteLine(num1);
-// System.Console.WriteLine(num2);
+// // // double num = random.NextDouble();
+// // System.Console.WriteLine(num);
+// // System.Console.WriteLine(num1);
+// // System.Console.WriteLine(num2);
 
-Console.WriteLine("Enter Side A: ");
-double a = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Enter Side A: ");
+// double a = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Enter Side B: ");
-double b = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Enter Side B: ");
+// double b = Convert.ToDouble(Console.ReadLine());
 
-double c = Math.Sqrt((a * a) + (b * b));
-System.Console.WriteLine("The hypotenuse is: " +c);
+// double c = Math.Sqrt((a * a) + (b * b));
+// System.Console.WriteLine("The hypotenuse is: " +c);
+
+String fullName = "Saral";
+fullName = fullName.ToUpper();
+fullName = fullName.ToLower();
+System.Console.WriteLine(fullName);
