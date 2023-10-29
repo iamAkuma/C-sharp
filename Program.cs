ba84ad891d62f762962dd2090da58ea651aa2b25
Saral Bajracharya <rmanbajra@gmail.com>
2023-10-29 16:32:21 +0545

    RPS game using C#

--- Program.cs
+++ Program.cs
@@ -327,59 +327,159 @@
 
 //Guess random number game
 
-Random random = new Random();
-bool playAgain = true;
-int min = 1;
-int max = 100;
-int guess;
-int number;
-int guesses;
-String response;
+// Random random = new Random();
+// bool playAgain = true;
+// int min = 1;
+// int max = 100;
+// int guess;
+// int number;
+// int guesses;
+// String response;
 
-while (playAgain)
-{
-    guess = 0;
-    guesses = 0;
-    response = "";
-    number = random.Next(min, max + 1);
+// while (playAgain)
+// {
+//     guess = 0;
+//     guesses = 0;
+//     response = "";
+//     number = random.Next(min, max + 1);
 
-    while (guess != number)
-    {
-        System.Console.WriteLine("Guess a number between " + min + "-" + max + ":");
-        guess = Convert.ToInt32(Console.ReadLine());
-        System.Console.WriteLine("Guess: " + guess);
+//     while (guess != number)
+//     {
+//         System.Console.WriteLine("Guess a number between " + min + "-" + max + ":");
+//         guess = Convert.ToInt32(Console.ReadLine());
+//         System.Console.WriteLine("Guess: " + guess);
 
-        if(guess > number)
-        {
-            System.Console.WriteLine(guess + " is too high");
-        }
-        else if(guess < number)
-        {
-            System.Console.WriteLine(guess + " is too low");
-        }
-        guesses++;
-    }
-    Console.WriteLine("Number: " +number);
-    System.Console.WriteLine("YOU WIN!!");;
-    System.Console.WriteLine("Guess: " +guess);
+//         if(guess > number)
+//         {
+//             System.Console.WriteLine(guess + " is too high");
+//         }
+//         else if(guess < number)
+//         {
+//             System.Console.WriteLine(guess + " is too low");
+//         }
+//         guesses++;
+//     }
+//     Console.WriteLine("Number: " +number);
+//     System.Console.WriteLine("YOU WIN!!");;
+//     System.Console.WriteLine("Guess: " +guess);
 
-    Console.WriteLine("Would you like to play again (Y/N): ");
-    response = Console.ReadLine();
-    response = response.ToUpper();
+//     Console.WriteLine("Would you like to play again (Y/N): ");
+//     response = Console.ReadLine();
+//     response = response.ToUpper();
 
-    if(response == "Y"){
-        playAgain = true;
-    }
-    else{
-        playAgain = false;
-    }
+//     if(response == "Y"){
+//         playAgain = true;
+//     }
+//     else{
+//         playAgain = false;
+//     }
 
-    if(response!="Y"){
-        System.Console.WriteLine("Thanks for playing");
-    }
-    else{
-        System.Console.WriteLine("Let's start the game again!");
-    }
+//     if(response!="Y"){
+//         System.Console.WriteLine("Thanks for playing");
+//     }
+//     else{
+//         System.Console.WriteLine("Let's start the game again!");
+//     }
 
 
-}
+// }
+
+// Rock Paper Scissors game
+
+ Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String computer;
+            String answer;
+
+            while (playAgain)
+            {
+                player = "";
+                computer = "";
+                answer = "";
+
+                while (player != "ROCK" && player != "PAPER" && player != "SCISSORS") {
+                    Console.Write("Enter ROCK, PAPER, or SCISSORS: ");
+                    player = Console.ReadLine();
+                    player = player.ToUpper();
+                }
+
+
+                switch (random.Next(1, 4))
+                {
+                    case 1:
+                        computer = "ROCK";
+                        break;
+                    case 2:
+                        computer = "PAPER";
+                        break;
+                    case 3:
+                        computer = "SCISSORS";
+                        break;
+                }
+
+                Console.WriteLine("Player: " + player);
+                Console.WriteLine("Computer: " + computer);
+
+                switch (player)
+                {
+                    case "ROCK":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        break;
+                    case "PAPER":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        break;
+                    case "SCISSORS":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        break;
+                }
+
+                Console.Write("Would you like to play again (Y/N): ");
+                answer = Console.ReadLine();
+                answer = answer.ToUpper();
+
+                if (answer == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+
+            }
+
+            Console.WriteLine("Thanks for playing!");
