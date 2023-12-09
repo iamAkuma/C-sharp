5442a2c502d129914746148a4d5fe750380d88a0
Saral Bajracharya <rmanbajra@gmail.com>
2023-12-09 18:31:30 +0545

    C# ASSIGNMENT

--- Program.cs
+++ Program.cs
@@ -1355,3 +1355,29 @@
 //         PrintArray(originalArray);
 //     }
 // }
+
+
+// Sum of first n numbers
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the value of n:");
+
+        // Read user input for n
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        // Calculate the sum
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+
+        // Display the result
+        Console.WriteLine($"The sum of the first {n} numbers is: {sum}");
+    }
+}
